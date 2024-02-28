2c6757eebce54ed8ebe7707bc30fa20ed59dabaf
corruptedbyte <152075924+corruptedbyte@users.noreply.github.com>
2024-02-28 15:07:58 +0200

    Create h1.cpp

---
+++ h1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	//cout << "Never gonna give you up" << endl << "Never gonna let you down" << endl << "Never gonna run around and desert you" << endl << "Never gonna make you cry" << endl << "Never gonna say goodbye" << endl << "Never gonna say goodbye" << endl << "Never gonna tell a lie and hurt you";
+	//cout << "Rainbow purple," << endl << "Rainbow blue" << endl << "Rainbow green and yellow, too." << endl << "Rainbow orange," << endl << "Rainbow red," << endl << "Raimbow colors on my head." << endl;
+	cout <<
+		"I'm selling puppies" << endl <<
+		"Breed: Labrador Retriever" << endl << endl <<
+		"Phone: 222-22-22" << endl <<
+		"222		| 222 |" << endl <<
+		"22		| 22 |" << endl <<
+		"22		| 22 |";
+}
