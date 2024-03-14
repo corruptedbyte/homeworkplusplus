48d13f257c477652df31de386667221c3994f04b
corruptedbyte <152075924+corruptedbyte@users.noreply.github.com>
2024-03-14 14:01:59 +0200

    Create h3-1.cpp

---
+++ h3-1.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int  main() {
+	/*int a;
+	int sum = 0;
+	cin >> a;
+
+	for (int i = a; i < 500; i++) {
+		sum = sum + i;
+	}
+	cout << sum << endl;*/
+	
+	/*int x, y;
+	cin >> x;
+	cin >> y;
+	cout << x << " ^ " << y << " = " << pow(x, y) << endl;*/
+
+	/*int sum = 0;
+
+	for (int i = 0; i < 1000; i++) {
+		sum = sum + i;
+	}
+
+	cout << "A. Mean of range (0, 1000): " << sum / 1000 << endl;*/
+
+	/*int multiplicator = 7;
+
+	for (int i = 1; i < 11; i++)
+	{
+		cout << multiplicator << " * " << i << " = " << i * multiplicator << endl;
+	}*/
+
+}
+
+// i've finally started uploading all that homework
