6a5e2c22f780f7015d7738797fd2b5bc52616f3f
corruptedbyte <152075924+corruptedbyte@users.noreply.github.com>
2024-03-14 13:31:15 +0200

    Update h2-1.cpp

--- h2-1.cpp
+++ h2-1.cpp
@@ -1,19 +1,59 @@
 #include <iostream>
+
 using namespace std;
 
-int main()
-{
-    /*int seconds = 86400;
-    int userSeconds;
-    cin >> userSeconds;
+int  main() {
+	//task 1
 
-    cout << "Hours: " << (seconds-userSeconds) / 3600 << endl;
-    cout << "Minutes: " << (seconds - userSeconds) / 60 << endl;
-    cout << "Seconds: " << (seconds - userSeconds) << endl;*/
+	/*int distance, time, speed;
+	cout << "Enter distance of the flight: ";
+	cin >> distance;
+	cout << endl;
+	cout << "Enter duration of the flight: ";
+	cin >> time;
+	cout << endl;
 
-    int hours = 28800;
-    int userSeconds;
-    cin >> userSeconds;
+	speed = distance / time;
 
-    cout << "Hours left: " << (hours - userSeconds) / 3600;
+	cout << "Approximate speed of your flight: " << speed << endl;*/
+
+	//task1 end
+
+	//task2
+
+	/*int cost = 2;
+	int hours, minutes, seconds;
+	int minutes_0, minutes_1, sum;
+
+	cout << "Enter hours spent: ";
+	cin >> hours;
+	cout << endl;
+	cout << "Enter minutes spent: ";
+	cin >> minutes;
+	cout << endl;
+	cout << "Enter seconds spent: ";
+	cin >> seconds;
+
+	minutes_0 = hours / 60;
+	minutes_1 = seconds * 60;
+
+	sum = minutes_0 + minutes_1 + minutes;
+
+	cout << "Your price: " << sum * 2;*/
+
+	//task2 end
+
+	//task3
+
+	int fuel0, fuel1, fuel2;
+
+	int distance, speed;
+
+	cin >> fuel0;
+	cin >> fuel1;
+	cin >> fuel2;
+
+
+	//task3 end
+
 }
